095ec059b0b13dfeb8ad1df861b9ab06e4e52123
HarshF40 <harshgaonkar57@gmail.com>
2025-02-28 15:43:26 +0530

    add backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/authRoutes.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,9 +16,8 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.json({message : "Hello World"});
-})
+app.get("/", (req, res) => { res.json({message : "Hello World"}); });
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server: Running on ${PORT}`)
